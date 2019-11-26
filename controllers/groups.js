e2b2c80b982e2620c448d657126422b700dc54e3
Fatairy <omar.elfatairy@student.guc.edu.eg>
2019-11-26 21:52:58 +0200

    getAallgroupsbyCreator and getAllGroupsByUser

--- controllers/groups.js
+++ controllers/groups.js
@@ -172,10 +172,122 @@ AcceptJoinRequest = async function(req, res) {
         res.status(422).send({ status: 'failure', message: 'Deletion of Group Failed' });
     }
 };
+
+JoinRequest = async function(req, res) {
+    try {
+        const validation = req.body && req.body.Name != null
+        if (!validation) {
+            return res.status(400).send({ status: 'failure', message: 'Params Missing' });
+
+        } else {
+            if (req.user_id !== req.params.user_id) {
+                return res.status(404).send({ status: 'failure', message: 'Access Forbidden' })
+            }
+            const user = await User.findOne({ '_id': req.params.user_id })
+            if (!user) {
+                return res.status(404).send({ status: 'failure', message: 'User Not Found' })
+            }
+
+            if (user.Deleted === true) { // Deactivated send message to activate
+                return res.status(404).send({ status: 'failure', message: 'Account is deactivated to activate your account please request access' })
+            }
+
+            const { Name } = req.body;
+            const group = await Group.findOne({ 'Name': Name });
+            const joinedGroup = await GroupUser.findOne({ group_id: group._id, user_id: req.user_id });
+            if (joinedGroup) {
+                if (joinedGroup.Pending)
+                    return res.status(404).send({ status: 'failure', message: 'You  Already  Requested to Enter This Group' });
+                else
+                    return res.status(404).send({ status: 'failure', message: 'You Are Already In This Group' });
+            } else {
+                const joining = await GroupUser.create({ group_id: group._id, user_id: req.user_id, Pending: true });
+                const joiningCreated = await GroupUser.findOne({ '_id': joining._id });
+                res.status(200).send({ status: 'success', msg: 'Join Request created successfully', data: joiningCreated });
+            }
+
+
+
+
+
+        }
+    } catch (e) {
+        console.log(e)
+        res.status(422).send({ status: 'failure', message: 'Deletion of Group Failed' });
+    }
+};
+GetAllGroupByCreator = async function(req, res) {
+    try {
+       
+            if (req.user_id !== req.params.user_id) {
+                return res.status(404).send({ status: 'failure', message: 'Access Forbidden' })
+            }
+            const user = await User.findOne({ '_id': req.params.user_id })
+            if (!user) {
+                return res.status(404).send({ status: 'failure', message: 'User Not Found' })
+            }
+            if (user.User_Category !== 'Doctor') {
+                return res.status(404).send({ status: 'failure', message: 'Access Forbidden' })
+            }
+
+            if (user.Deleted === true) { // Deactivated send message to activate
+                return res.status(404).send({ status: 'failure', message: 'Account is deactivated to activate your account please request access' })
+            }
+
+            
+
+           const allGroupsCreatedByMe = await Group.find({Created_By : req.user_id})
+         return res.status(200).send({ status: 'success', message: 'success', data: allGroupsCreatedByMe });
+        
+    } catch (e) {
+        console.log(e)
+        res.status(422).send({ status: 'failure', message: ' Showing Group Created By Me Failed' });
+    }
+};
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
+GetAllGroupByUser = async function(req, res) {
+    try {
+       
+            if (req.user_id !== req.params.user_id) {
+                return res.status(404).send({ status: 'failure', message: 'Access Forbidden' })
+            }
+            const user = await User.findOne({ '_id': req.params.user_id })
+            if (!user) {
+                return res.status(404).send({ status: 'failure', message: 'User Not Found' })
+            }
+            if (user.Deleted === true) { // Deactivated send message to activate
+                return res.status(404).send({ status: 'failure', message: 'Account is deactivated to activate your account please request access' })
+            }
+
+            
+
+           const allGroupsUserJoined = await GroupUser.find({user_id : req.user_id, Pending:false})
+           var allGroups =[];
+           allGroupsUserJoined.map( async (group, i) => {
+            const currentGroup =  await Group.findOne({ '_id': group.group_id })
+                allGroups[i] = currentGroup;
+                console.log(allGroups)
+            
+            
+        })
+        await sleep(2000);
+        return res.status(200).send({ status: 'success', message: 'success', data: allGroups });
+        
+    } catch (e) {
+        console.log(e)
+        res.status(422).send({ status: 'failure', message: ' Showing Group Users Me Failed' });
+    }
+};
 module.exports = {
     CreateGroup,
     DeleteGroup,
     GetALlGroups,
     JoinRequest,
-    AcceptJoinRequest
+    AcceptJoinRequest,
+    GetAllGroupByCreator,
+    GetAllGroupByUser
 }
