7c4c782dd3038b1c44a09bd183b94f4f8875a248
madoooaboelazaiem <madooo@hotmail.com>
2019-11-26 12:06:59 +0200

    Pulled sprints/01

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const reply = require('./routes/replies')
 require('dotenv').config();
 mongoose.Promise = global.Promise;
 mongoose
-    .connect(db2, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err))
 app.use(express.json())
